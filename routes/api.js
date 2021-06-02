a612c289c67f075583a0e40ad09bb80aea06d230
Jordan <jaydubwa@gmail.com>
2021-06-02 15:15:17 -0400

    made minor changes

--- routes/api.js
+++ routes/api.js
@@ -1,11 +1,11 @@
 'use strict';
 const axios = require('axios').default;
 const dbase = require('../dbase')
-console.log(dbase.findOneAndUpdateDB({ip:'1.1.1.4',stocks:[{stock:'msft',likes:1},{stock:'GOOG',likes:1}]}));
+//console.log(dbase.findOneAndUpdateDB({ip:'1.1.1.4',stocks:[{stock:'msft',likes:1},{stock:'GOOG',likes:1}]}));
 
 module.exports = function(app) {
 
-  const getStock = async (stock, like) => {
+  const getStock = async (stock, like,res) => {
 			try {
 				const response = await axios.get(
 					`https://stock-price-checker-proxy.freecodecamp.rocks/v1/stock/${stock}/quote`
@@ -13,23 +13,25 @@ module.exports = function(app) {
 			console.log(response.data.latestPrice);
         return {stock:response.data.symbol,price:response.data.latestPrice, like}
 			} catch (error) {
-				console.error(error)
+				console.error(error.message)
         res.status(404).json({Msg:error.message, Details:`${stock} is not a valid stock symbol`})
 			}
 		}
 	app.route('/api/stock-prices',).get(async (req,res) =>{
 		const IP = req.ip;
-    const like = req.query.like
     console.log(IP,req.query);
     
     if (Array.isArray(req.query.stock)){
       let results = [];
       req.query.stock.forEach(async(stock,i,arr)=>{
-        results.push(await getStock(stock, like))
-        if(i === arr.length-1)res.json(results)
+        results.push(await getStock(stock,req.query.like,res))
+        if(i === arr.length-1 || i > 4)res.json(results)
       })
-    }else{
-    const stockData = await getStock(req.query.stock,like);
+    //   try{
+    //  (async(results)=>console.log(await results))();
+    //   }catch(err) {console.log(err)}
+    // }else{
+    const stockData = await getStock(req.query.stock,req.query.like,res);
       res.json({stockData})
     }
 	})
