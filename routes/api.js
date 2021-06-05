01d628b10b1cfc4f687b6ab9f7a8624f20b5e943
Jfoobar <jaydubwa@gmail.com>
2021-06-05 08:35:55 -0400

    changed async foreach method to for of loop with async call

--- routes/api.js
+++ routes/api.js
@@ -1,17 +1,18 @@
 'use strict';
 const axios = require('axios').default;
 const dbase = require('../dbase')
-//console.log(dbase.findOneAndUpdateDB({ip:'1.1.1.4',stocks:[{stock:'msft',likes:1},{stock:'GOOG',likes:1}]}));
+//console.log(dbase.findOneAndUpdateDB({
+//ip:'1.1.1.4',stocks:[{stock:'msft',likes:1},{stock:'GOOG',likes:1}]}));
 
 module.exports = function(app) {
 
-  const getStock = async (stock, like,res) => {
+  const getStock = async (stock,res) => {
 			try {
 				const response = await axios.get(
 					`https://stock-price-checker-proxy.freecodecamp.rocks/v1/stock/${stock}/quote`
 				);
-			console.log(response.data.latestPrice);
-        return {stock:response.data.symbol,price:response.data.latestPrice, like}
+			  console.log(response.data.latestPrice);
+        return {stock:response.data.symbol,price:response.data.latestPrice}
 			} catch (error) {
 				console.error(error.message)
         res.status(404).json({Msg:error.message, Details:`${stock} is not a valid stock symbol`})
@@ -22,16 +23,20 @@ module.exports = function(app) {
     console.log(IP,req.query);
     
     if (Array.isArray(req.query.stock)){
-      let results = [];
-      req.query.stock.forEach(async(stock,i,arr)=>{
-        results.push(await getStock(stock,req.query.like,res))
-        if(i === arr.length-1 || i > 4)res.json(results)
-      })
-    //   try{
-    //  (async(results)=>console.log(await results))();
-    //   }catch(err) {console.log(err)}
+      const asyncLoop = async ()=>{
+        let results = [];
+        for(let stock of req.query.stock){
+          let stockInfo = await getStock(stock)
+          results.push(stockInfo)
+        }
+        return results;
+    }
+    const stockData = await asyncLoop();
+    if(req.query.like)
+    res.json({stockData})
+
     }else{
-    const stockData = await getStock(req.query.stock,req.query.like,res);
+      const stockData = await getStock(req.query.stock,res);
       res.json({stockData})
     }
 	})
