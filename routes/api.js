2971131a5699f8ed14a276cea7cae13c53a6560d
Jordan Wendell <37525787+Jfoobar@users.noreply.github.com>
2021-06-20 12:43:46 -0400

    Cleaned format a bit

--- routes/api.js
+++ routes/api.js
@@ -1,58 +1,57 @@
 'use strict';
 const axios = require('axios').default;
 const dbase = require('../dbase')
-//console.log(dbase.findOneAndUpdateDB({
-//ip:'1.1.1.4',stocks:[{stock:'msft',likes:1},{stock:'GOOG',likes:1}]}));
 
 module.exports = function(app) {
   const getStock = async (stock,res) => {
-	  try {
-			const response = await axios.get(
-				`https://stock-price-checker-proxy.freecodecamp.rocks/v1/stock/${stock}/quote`);
+     try {
+	const response = await axios.get(
+	`https://stock-price-checker-proxy.freecodecamp.rocks/v1/stock/${stock}/quote`);
         const {symbol,latestPrice} = response.data;
         return {stock:symbol,price:latestPrice};
-		}catch (error) {
-				console.error(error.message);
-        res.status(404).json({Msg:error.message, Details:`${stock} is not a valid stock symbol`});
-			}
-		}
-	app.route('/api/stock-prices',).get(async (req,res) =>{
-		const IP = req.ip;
-    const symbol = req.query.stock;
+	  }catch (error) {
+	    console.error(error.message);
+            res.status(404).json({
+              Msg:error.message, Details:`${stock} is not a valid stock symbol`});
+               }
+	     }
+        app.route('/api/stock-prices',).get(async (req,res) =>{
+	  const IP = req.ip;
+          const symbol = req.query.stock;
 
-    if (Array.isArray(symbol)){
-      const asyncLoop = async ()=>{
-        let results = [];
-        const arr = symbol;
-        let tempArr = [];
-        for(let x in arr){
-          if (x>2)break;
-          let stockInfo = await getStock(arr[x]);
-          results.push(stockInfo)
-          if(req.query.like ==='true'){
-          tempArr[x] = await dbase.findOneAndUpdateDB({ip:IP,stocks:
-          [{stock:arr[x].toUpperCase(),likes:1}]});
-          }else{
-            tempArr[x] = await dbase.findOneAndUpdateDB({ip:IP,stocks:
-              [{stock:arr[x].toUpperCase()}]});
-          }
-        }
-        results[0].rel_likes = tempArr[0]-tempArr[1]
-        results[1].rel_likes = tempArr[1]-tempArr[0]
-        return results;
-      }
-      const stockData = await asyncLoop();
-      res.json({stockData});
-    }else{
-      const stockData = await getStock(symbol,res);
-      if(req.query.like ==='true'){
-        stockData.likes = await dbase.findOneAndUpdateDB({ip:IP,stocks:
-          [{stock:symbol.toUpperCase(),likes:1}]});
-      }else{
-        stockData.likes = await dbase.findOneAndUpdateDB({ip:IP,stocks:
-          [{stock:symbol.toUpperCase()}]})
-      }
-      res.json({stockData});
+          if (Array.isArray(symbol)){
+            const asyncLoop = async ()=>{
+              let results = [];
+              const arr = symbol;
+              let tempArr = [];
+              for(let x in arr){
+                if (x>2)break;
+                let stockInfo = await getStock(arr[x]);
+                results.push(stockInfo)
+                if(req.query.like ==='true'){
+                  tempArr[x] = await dbase.findOneAndUpdateDB({ip:IP,stocks:
+                  [{stock:arr[x].toUpperCase(),likes:1}]});
+                  }else{
+                    tempArr[x] = await dbase.findOneAndUpdateDB({ip:IP,stocks:
+                    [{stock:arr[x].toUpperCase()}]});
+                    }
+                  }
+              results[0].rel_likes = tempArr[0]-tempArr[1]
+              results[1].rel_likes = tempArr[1]-tempArr[0]
+              return results;
+              }
+            const stockData = await asyncLoop();
+            res.json({stockData});
+            }else{
+              const stockData = await getStock(symbol,res);
+              if(req.query.like ==='true'){
+                stockData.likes = await dbase.findOneAndUpdateDB({ip:IP,stocks:
+                [{stock:symbol.toUpperCase(),likes:1}]});
+              }else{
+                stockData.likes = await dbase.findOneAndUpdateDB({ip:IP,stocks:
+                [{stock:symbol.toUpperCase()}]})
+              }
+       res.json({stockData});
     }
-	})
+  })
 };
